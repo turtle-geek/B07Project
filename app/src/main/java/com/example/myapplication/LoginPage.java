52797a7f284237ccd9e50a9d001b6ac4bdd08b7e
RachelWong8182 <rachelwong8182@gmail.com>
2025-11-17 15:11:29 -0500

    feat:main page, login pages update, adding children function page

--- app/src/main/java/com/example/myapplication/LoginPage.java
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -1,11 +1,9 @@
 package com.example.myapplication;
 
-import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textfield.TextInputEditText;
@@ -16,7 +14,7 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.layout_login);
 
         // Initialize text fields
         TextInputLayout mailLayout = findViewById(R.id.mailLayoutLogin);
--- app/src/main/java/com/example/myapplication/SignOut.java
+++ app/src/main/java/com/example/myapplication/SignOut.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +12,7 @@ public class SignOut extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signout_page1);
+        setContentView(R.layout.layout_signout);
 
 
         authManager = new AuthManager();
