e2b97048b198dc34af635576d8ad80c564bf3842
Cindy Xing <jinqingxing22@gmail.com>
2025-11-18 12:18:46 -0500

    feat: work on switch page depends on role

--- app/src/main/java/com/example/loginapp/ChildHomeActivity.java
+++ app/src/main/java/com/example/loginapp/ChildHomeActivity.java
@@ -9,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
