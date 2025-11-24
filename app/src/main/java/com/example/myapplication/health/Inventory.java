6317e1badde362c89bb20a4aa550c0c8f8454d86
jackyleunghh <jackyleunghh@gmail.com>
2025-11-24 14:34:55 -0500

    feat: updated Inventory and InventoryItem

--- app/src/main/java/com/example/myapplication/health/Inventory.java
+++ app/src/main/java/com/example/myapplication/health/Inventory.java
@@ -4,4 +4,28 @@ import java.util.ArrayList;
 
 public class Inventory {
     private ArrayList<InventoryItem> inventory;
+
+    public Inventory() {
+        inventory = new ArrayList<>();
+    }
+
+    public void addItem(InventoryItem medicine) {
+        inventory.add(medicine);
+    }
+
+    public boolean useMedicine(int index, double amount) {
+        if (index < 0 || index >= inventory.size())
+            return false;
+        InventoryItem medicine = inventory.get(index);
+        if (amount > medicine.getAmount())
+            return false;
+        else {
+            medicine.setAmount(medicine.getAmount() - amount);
+            if (medicine.getAmount() == 0)
+                inventory.remove(medicine);
+            return true;
+        }
+    }
+
+    
 }
--- app/src/main/java/com/example/myapplication/health/InventoryItem.java
+++ app/src/main/java/com/example/myapplication/health/InventoryItem.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 
 public class InventoryItem {
     private double amount;
+    private final double capacity;
     private final LocalDateTime purchaseDate;
     private final LocalDateTime expiryDate;
 
-    public InventoryItem(double amount, LocalDateTime purchaseDate, LocalDateTime expiryDate) {
+    public InventoryItem(double amount, double capacity, LocalDateTime purchaseDate, LocalDateTime expiryDate) {
         this.amount = amount;
+        this.capacity = capacity;
         this.purchaseDate = purchaseDate;
         this.expiryDate = expiryDate;
     }
@@ -21,11 +23,23 @@ public class InventoryItem {
         this.amount = amount;
     }
 
+    public double getCapacity() {
+        return capacity;
+    }
+
     public LocalDateTime getExpiryDate() {
         return expiryDate;
     }
 
     public LocalDateTime getPurchaseDate() {
         return purchaseDate;
+    }
+
+    public boolean lowVolumeAlert() {
+        return amount / capacity <= 0.2;
+    }
+
+    public boolean expiryAlert() {
+        return LocalDateTime.now().compareTo(expiryDate.minusWeeks(4)) > 0; // 4 weeks before expiry
     }
 }
