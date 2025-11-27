03190caa71530b76243b22a82ea53bea1921cc26
jackyleunghh <jackyleunghh@gmail.com>
2025-11-27 01:22:27 -0500

    fix: refactored Inventory related logic for having one canister for each controller and rescus (from Piazza)

--- app/src/main/java/com/example/myapplication/health/MedicineLabel.java
+++ app/src/main/java/com/example/myapplication/health/MedicineLabel.java
@@ -2,5 +2,5 @@ package com.example.myapplication.health;
 
 public enum MedicineLabel {
     CONTROLLER,
-    RESCUE;
+    RESCUE
 }
--- app/src/main/java/com/example/myapplication/models/TechniqueQuality.java
+++ app/src/main/java/com/example/myapplication/models/TechniqueQuality.java
@@ -4,5 +4,5 @@ public enum TechniqueQuality {
     HIGH,
     MEDIUM,
     LOW,
-    NA;
+    NA
 }
