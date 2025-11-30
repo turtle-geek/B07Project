d8da79f6f4d5552637c03eafa55bf22d76f7200a
RachelWong8182 <rachelwong8182@gmail.com>
2025-11-30 04:23:40 -0500

    feat: changed the technique help layout, prepost will be popup on child home page after technique help

--- app/src/main/java/com/example/myapplication/ui/ChildHomeActivity.java
+++ app/src/main/java/com/example/myapplication/ui/ChildHomeActivity.java
@@ -5,10 +5,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +25,8 @@ import jp.wasabeef.blurry.Blurry;
 
 public class ChildHomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private View prepostCheckPopup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +38,60 @@ public class ChildHomeActivity extends AppCompatActivity {
         });
         mAuth = FirebaseAuth.getInstance();
 
+        // Initialize prepost check popup
+        prepostCheckPopup = findViewById(R.id.prepostCheckPopup);
+
+        // Check if we should show the prepost check
+        if (getIntent().getBooleanExtra("SHOW_PREPOST_CHECK", false)) {
+            showPrePostCheck();
+        }
+
+        // Set up button listeners for the prepost check
+        setupPrePostCheckButtons();
     }
+
+    private void showPrePostCheck() {
+        if (prepostCheckPopup != null) {
+            prepostCheckPopup.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hidePrePostCheck() {
+        if (prepostCheckPopup != null) {
+            prepostCheckPopup.setVisibility(View.GONE);
+        }
+    }
+
+    private void setupPrePostCheckButtons() {
+        ImageButton betterBtn = findViewById(R.id.better);
+        ImageButton sameBtn = findViewById(R.id.same);
+        ImageButton worseBtn = findViewById(R.id.Worse);
+
+        if (betterBtn != null) {
+            betterBtn.setOnClickListener(v -> {
+                // Handle "Better" selection
+                // TODO: Save the response to database
+                hidePrePostCheck();
+            });
+        }
+
+        if (sameBtn != null) {
+            sameBtn.setOnClickListener(v -> {
+                // Handle "Same" selection
+                // TODO: Save the response to database
+                hidePrePostCheck();
+            });
+        }
+
+        if (worseBtn != null) {
+            worseBtn.setOnClickListener(v -> {
+                // Handle "Worse" selection
+                // TODO: Save the response to database
+                hidePrePostCheck();
+            });
+        }
+    }
+
     //This method is to block users' access to visit this app's Child Home Activities,
     // if they don't have an account
     @Override
